4a613bfb253f651aa37edc8d85f33fd60d143b46
Akram Gafurov <akramminsk@yandex.by>
2022-06-01 00:23:33 +0300

    feat: add 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+let data = ''
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+readStream.on('data', chunk => data += chunk)
+readStream.on('end', () => console.log(data))
